b013c7708875a822db8a82df47efd5c4bf5a1dff
Ajiboyeniola <ajiboyeniola@gmail.com>
2024-03-07 14:52:41 +0100

    changes

---
+++ client/app/vote/page.jsx
@@ -0,0 +1,12 @@
+import VotersCard from "@/components/VotersCard";
+import React from "react";
+
+const Vote = () => {
+  return (
+    <div className="">
+      <VotersCard />
+    </div>
+  );
+};
+
+export default Vote;
---
+++ client/components/VotersCard.jsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const VotersCard = () => {
+  return <div>VotersCard</div>;
+};
+
+export default VotersCard;
