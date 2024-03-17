5fe9c75f67c428fdf64eda5041df2864c767ec6f
Ajiboyeniola <ajiboyeniola@gmail.com>
2024-03-17 14:54:47 +0100

    FT: Voters Social Handle Details

--- client/components/VoteCard.jsx
+++ client/components/VoteCard.jsx
@@ -13,6 +13,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { set } from "react-hook-form";
+import VotersHandle from "./voters";
 
 const cards = [
   {
@@ -20,51 +21,60 @@ const cards = [
     musicName: "Melody Masters",
     message:
       "Lorem ipsum dolor sit amet id in ac in consectetur. Imperdiet facilisis id in ac enim pulvinar aenean.",
-    votes: "38",
+    votes: "0",
   },
   {
     image: "image2.jpg",
     musicName: "Harmonic Showcase",
     message:
       "Lorem ipsum dolor sit amet id in ac in consectetur. Imperdiet facilisis id in ac enim pulvinar aenean.",
-    votes: "38",
+    votes: "0",
   },
   {
     image: "image3.jpg",
     musicName: "Rhythm Rivalry",
     message:
       "Lorem ipsum dolor sit amet id in ac in consectetur. Imperdiet facilisis id in ac enim pulvinar aenean.",
-    votes: "38",
+    votes: "0",
   },
   {
     image: "image4.jpg",
     musicName: "Songbird Showdown",
     message:
       "Lorem ipsum dolor sit amet id in ac in consectetur. Imperdiet facilisis id in ac enim pulvinar aenean.",
-    votes: "38",
+    votes: "0",
   },
   {
     image: "image5.jpg",
     musicName: "Crescendo Challenge",
     message:
       "Lorem ipsum dolor sit amet id in ac in consectetur. Imperdiet facilisis id in ac enim pulvinar aenean.",
-    votes: "38",
+    votes: "0",
   },
   {
     image: "image6.jpg",
     musicName: "Tune Triumph",
     message:
       "Lorem ipsum dolor sit amet id in ac in consectetur. Imperdiet facilisis id in ac enim pulvinar aenean.",
-    votes: "38",
+    votes: "0",
   },
 ];
 
 const VoteCard = () => {
+  const [showHandle, setShowHandle] = useState(false);
   const [data, setData] = useState(null);
-  const [isloading, isLoading] = useState(true);
+  const [isloading, setIsLoading] = useState(true);
   const [requestLog, setRequestLog] = useState("");
   const [responseLog, setResponseLog] = useState("");
   const [errorLog, setErrorLog] = useState("");
+
+  const handleVote = () => {
+    setShowHandle(true);
+  };
+
+  const handleClose = () => {
+    setShowHandle(false);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -75,11 +85,13 @@ const VoteCard = () => {
         const response = await axios.fetch(apiURL);
         setResponseLog(`Participants data: ${JSON.stringify(response.data)}`);
         setData(response.data);
+        setIsLoading(false);
       } catch (error) {
         const errorMessage = error.response
           ? JSON.stringify(error.response.data)
           : error.message;
         setErrorLog(errorMessage);
+        setIsLoading(false);
         console.error("Error fetching data:", error);
       }
     };
@@ -88,31 +100,48 @@ const VoteCard = () => {
   }, []);
 
   return (
-    <div className="flex justify-center flex-wrap gap-6 w-full">
-      {cards.map((card, i) => (
-        <Card className="bg-white sm:w-auto w-3/4 rounded-xl flex-col gap-4">
-          <CardHeader key={i} className="px-0 pt-0 pb-0">
-            <Image
-              src={`/assets/images/${card.image}`}
-              alt="flower"
-              width={235}
-              height={175}
-              className="object-cover rounded-t-[12px] lg:w-[235px] w-full h-[175px]"
-            />
-            <CardDescription className="sm:w-[235px] w-full text-base font-semibold font-mont px-2 pt-2">
-              {card.musicName}
-            </CardDescription>
-          </CardHeader>
+    <div className="relative">
+      <div className="flex justify-center flex-wrap gap-6 w-full">
+        {cards.map((card, i) => (
+          <Card
+            className="bg-white sm:w-auto w-3/4 rounded-xl flex-col gap-4"
+            key={i}
+          >
+            {/* Your card content */}
+            <CardHeader className="px-0 pt-0 pb-0">
+              <Image
+                src={`/assets/images/${card.image}`}
+                alt="flower"
+                width={235}
+                height={175}
+                className="object-cover rounded-t-[12px] lg:w-[235px] w-full h-[175px]"
+              />
+              <CardDescription className="sm:w-[235px] w-full text-base font-semibold font-mont px-2 pt-2">
+                {card.musicName}
+              </CardDescription>
+            </CardHeader>
 
-          <CardContent className="flex-col justify-between px-2 pt-0 pb-0">
-            <p className="text-sm">{card.votes} votes</p>
-          </CardContent>
+            <CardContent className="flex-col justify-between px-2 pt-0 pb-0">
+              <p className="text-sm">{card.votes} votes</p>
+            </CardContent>
 
-          <CardFooter className="px-2 pt-4 pb-2">
-            <Button className="yellow_btn w-full">Vote</Button>
-          </CardFooter>
-        </Card>
-      ))}
+            <CardFooter className="px-2 pt-4 pb-2">
+              <Button
+                className="yellow_btn w-full"
+                onClick={() => handleVote()}
+              >
+                Vote
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+
+      {showHandle && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80 z-50">
+          <VotersHandle handleClose={handleClose} />
+        </div>
+      )}
     </div>
   );
 };
--- client/components/voters.jsx
+++ client/components/voters.jsx
@@ -1,7 +1,168 @@
-import React from "react";
+"use client";
 
-const voters = () => {
-  return <div>voters</div>;
+import axios from "axios";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectGroup,
+  SelectLabel,
+  SelectValue,
+} from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+
+const socialMediaOptions = [
+  { value: "facebook", label: "Facebook" },
+  { value: "instagram", label: "Instagram" },
+  { value: "tiktok", label: "TikTok" },
+];
+
+const VotersHandle = ({ handleClose }) => {
+  const [formValidation, setFormValidation] = useState(false);
+  const [formValues, setFormValues] = useState({
+    socialMediaHandle: "",
+    socialMediaPlatform: "",
+  });
+  const [selectedValue, setSelectedValue] = useState("");
+
+  const validateForm = (values) => {
+    let isValid = true;
+
+    if (values.socialMediaHandle.trim() === "") {
+      isValid = false;
+      console.log("Social handle is empty");
+    }
+    if (!selectedValue) {
+      isValid = false;
+      console.log("Social media is not picked");
+    }
+    return isValid;
+  };
+
+  const handleVote = () => {
+    const isValid = validateForm(formValues);
+    if (!isValid) {
+      console.error("Form validation failed");
+      return; // Stop the form submission if validation fails
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value, type, checked, files } = e.target;
+    const val =
+      type === "checkbox" ? checked : type === "file" ? files[0] : value;
+
+    setFormValues((prevState) => ({
+      ...prevState,
+      [name]: val,
+    }));
+  };
+
+  const closeHandle = () => {
+    handleClose();
+  };
+
+  const handleSocialMediaSelect = (event) => {
+    setSelectedValue(event);
+    setFormValues((prevState) => ({
+      ...prevState,
+      socialMediaPlatform: event,
+    }));
+  };
+  return (
+    <div
+      className="p-[12px] rounded-xl shadow-2xl"
+      style={{
+        backgroundColor: "rgba(255, 255, 255, 0.1)",
+        backdropFilter: "blur(14px)",
+      }}
+    >
+      <Card className="card">
+        <CardHeader>
+          <div className="flex justify-between">
+            <CardTitle className="head_text w-auto sm:w-[484px]">
+              Vote Participants
+            </CardTitle>
+            <img
+              src="/assets/icons/menu.svg"
+              alt="menu"
+              width={32}
+              height={32}
+              className="cursor-pointer"
+              onClick={closeHandle}
+            />
+          </div>
+          <CardDescription className="head_para">
+            Enter your preferred social Handle to vote your favorite.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="socialMediaPlatform" className="label">
+                  Social Media Platform
+                </Label>
+                <Select
+                  onValueChange={(event) => handleSocialMediaSelect(event)}
+                >
+                  <SelectTrigger className=" input">
+                    <SelectValue placeholder="Select platform" />
+                  </SelectTrigger>
+                  <SelectContent className="bg-white">
+                    <SelectGroup>
+                      <SelectLabel>Social Handle</SelectLabel>
+
+                      {socialMediaOptions.map((social, i) => (
+                        <SelectItem key={i} value={social.label}>
+                          {social.label}
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name" className="label">
+                  Social Media Handle
+                </Label>
+                <Input
+                  id="socialMediaHandle"
+                  name="socialMediaHandle"
+                  placeholder="Enter your social media handle"
+                  className="input "
+                  value={formValues.socialMediaHandle}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button
+            type="submit"
+            className="yellow_btn w-full"
+            onClick={handleVote}
+          >
+            Vote
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 };
 
-export default voters;
+export default VotersHandle;
