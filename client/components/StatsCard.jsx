c26a6e42e1a378a5b81d4b61ca413ad356d6ad39
Ajiboyeniola <ajiboyeniola@gmail.com>
2024-04-09 19:39:28 +0100

    ft API added

--- client/components/StatsCard.jsx
+++ client/components/StatsCard.jsx
@@ -1,29 +1,71 @@
-import React from "react";
-
-const cards = [
-  {
-    title: "Total Participants",
-    numbers: 0,
-    icon: "people.svg",
-  },
-  {
-    title: "Total approved entries",
-    numbers: 0,
-    icon: "tick-circle.svg",
-  },
-  {
-    title: "Total pending entries",
-    numbers: 0,
-    icon: "danger.svg",
-  },
-  {
-    title: "Total declined entries",
-    numbers: 0,
-    icon: "close-circle.svg",
-  },
-];
+"use client";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const StatsCard = () => {
+  const [totalParticipants, setTotalParticipants] = useState(0);
+  const [totalApprovedEntries, setTotalApprovedEntries] = useState(0);
+  const [totalPendingEntries, setTotalPendingEntries] = useState(0);
+  const [totalDeclineEntries, setTotalDeclineEntries] = useState(0);
+  const [isLoad, stats] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const apiBaseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+      const apiURLTotal = `${apiBaseURL}/admin/total-participant`;
+      const apiURLApproved = `${apiBaseURL}/admin/approve-entries`;
+      const apiURLDecline = `${apiBaseURL}/admin/decline/:participantId`;
+
+      try {
+        const [
+          totalParticipantsResponse,
+          totalApprovedEntriesResponse,
+          totalPendingEntriesResponse,
+          totalDeclinedEntriesResponse,
+        ] = await Promise.all([
+          axios.get(apiURLTotal),
+          axios.get(apiURLApproved),
+          axios.get(apiURLDecline),
+        ]);
+        setTotalParticipants(totalParticipantsResponse.data.totalParticipants);
+        console.log(setTotalParticipants);
+        setTotalApprovedEntries(
+          totalApprovedEntriesResponse.data.totalApprovedEntries
+        );
+        setTotalDeclineEntries(
+          totalDeclinedEntriesResponse.data.totalDeclineEntries
+        );
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const cards = [
+    {
+      title: "Total Participants",
+      numbers: totalParticipants ? "Loading..." : totalParticipants,
+      icon: "people.svg",
+    },
+    {
+      title: "Total approved entries",
+      numbers: totalApprovedEntries,
+      icon: "tick-circle.svg",
+    },
+    {
+      title: "Total pending entries",
+      numbers: totalPendingEntries,
+      icon: "danger.svg",
+    },
+    {
+      title: "Total declined entries",
+      numbers: totalDeclineEntries,
+      icon: "close-circle.svg",
+    },
+  ];
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 justify-center place-content-start gap-4 text-white mb-10">
       {cards.map((card, i) => (
