62999c97c3e56a55e9f100a646c60af894c068db
Newman-Nheeon <newmanenyoojo@gmail.com>
2024-10-17 17:39:01 +0100

    ft: cors config modified

--- server/config/corsConfig.js
+++ server/config/corsConfig.js
@@ -1,7 +1,9 @@
 const logger = require('../logger');
+
+// Whitelist for allowed origins
 const whitelist = {
-  development: 'http://localhost:3000', // URL of your frontend in development
-  production: process.env.FRONTEND_PROD_URL // URL of your frontend in production
+  development: ['http://localhost:3000'], // Array of URLs for development
+  production: [process.env.FRONTEND_PROD_URL || 'https://gbd.nyeusi.org'] // Array for production
 };
 
 // Determine the current environment
@@ -10,6 +12,7 @@ const currentEnv = process.env.NODE_ENV || 'development';
 // CORS options
 const corsOptions = {
   origin: function (origin, callback) {
+    // Uncomment for debugging
     // logger.info(`Current environment is: ${currentEnv}`);
     // logger.info(`Whitelist for ${currentEnv}: ${whitelist[currentEnv]}`);
     // logger.info(`Whitelist type: ${whitelist[currentEnv].constructor.name}`);
@@ -18,16 +21,19 @@ const corsOptions = {
     // logger.info(`Check origin in whitelist: ${whitelist[currentEnv].indexOf(origin)}`);
     // logger.info(`Test specific string: "https://gbd.nyeusi.org".indexOf("https://gbd.nyeusi.org") = ${"https://gbd.nyeusi.org".indexOf("https://gbd.nyeusi.org")}`);
     
-    if (whitelist[currentEnv].indexOf(origin) !== -1 || !origin) {
+    const allowedOrigins = whitelist[currentEnv]; // Get the list of allowed origins for the current environment
+
+    // Check if the origin is in the whitelist
+    if (allowedOrigins.includes(origin) || !origin) {
       logger.info(`[CORS] Origin allowed: ${origin}`);
-      callback(null, true); // Allow if the origin is in the whitelist or server-to-server requests (no origin)
+      callback(null, true); // Allow if origin is in the whitelist or if it's a server-to-server request (no origin)
     } else {
       logger.info(`[CORS] Origin not allowed: ${origin}`);
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error('Not allowed by CORS')); // Deny the request
     }
   },
-  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
-  credentials: true, // Credentials are supported (cookies, HTTP authentication)
+  optionsSuccessStatus: 200, // Legacy browsers (IE11, etc.) choke on 204
+  credentials: true, // Allow credentials (cookies, HTTP authentication)
 };
 
 module.exports = corsOptions;
