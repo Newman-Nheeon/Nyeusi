0b4be25dce009f24eee48572ea2d279840ff59a1
Newman-Nheeon <newmanenyoojo@gmail.com>
2024-10-18 17:29:05 +0100

    ft: minor changes

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -23,7 +23,13 @@ exports.sendVerificationEmail = async (email, token) => {
     }, 
       to: email, // List of receivers
       subject: 'Verify Your Email Address', // Subject line
-    html: `<p>Please click on the link to verify your email: <a href="${verificationLink}">Verify Email</a></p>`,
+      html: `
+      <p>Thank you for signing up for the Nyeusi Give Black December Music Competition!</p>
+      <p>To complete your registration, please follow this link: <a href="${verificationLink}">Verify Email</a></p>
+      <p>Or copy and paste the following URL into your browser: ${verificationLink}</p>
+      <p>NOTE: If this email landed in your spam folder, please mark it as "not spam" and move it to your inbox before proceeding.</p>
+    `,
+    
   };
 
   await transporter.sendMail(mailOptions);
