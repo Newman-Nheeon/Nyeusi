7e43aafee25c4aede5481314ed355ff9a3153cee
Newman-Nheeon <newmanenyoojo@gmail.com>
2024-11-10 21:49:59 +0100

    ft: sendEmail

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -7,7 +7,7 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: true, // true for 465, false for other ports
+    secure: false, // true for 465, false for other ports
     auth: {
       user: process.env.EMAIL_USER, // Your email
       pass: process.env.EMAIL_PASSWORD, // Your email account password or app password
